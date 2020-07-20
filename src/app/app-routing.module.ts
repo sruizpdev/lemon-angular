8d15044cc3bfa3ff9178e0f3b94526fd42ba1db5
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-07-20 20:10:13 +0200

    mas opciones al menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { LoginComponent } from "./components/login/login.component";
 import { AboutComponent } from "./components/about/about.component";
+import { GaleriaComponent } from "./components/galeria/galeria.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { CrudComponent } from "./components/crud/crud.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "login", component: LoginComponent },
   { path: "about", component: AboutComponent },
+  { path: "galeria", component: GaleriaComponent },
+  { path: "crud", component: CrudComponent },
+  { path: "profile", component: ProfileComponent },
+  { path: "dashboard", component: DashboardComponent },
   { path: "**", pathMatch: "full", redirectTo: "home" },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,27 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatSliderModule } from "@angular/material/slider";
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
+import { AboutComponent } from "./components/about/about.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { CrudComponent } from "./components/crud/crud.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { GaleriaComponent } from "./components/galeria/galeria.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, NavbarComponent, AboutComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    AboutComponent,
+    DashboardComponent,
+    CrudComponent,
+    ProfileComponent,
+    GaleriaComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
